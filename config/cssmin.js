24d5a29f26434a4d7caaa32177251d4cfa4a7ef1
Josebaseba <josebalega@gmail.com>
2014-12-05 12:49:47 +0100

    Fixed cssmin version issue

--- config/cssmin.js
+++ config/cssmin.js
@@ -6,18 +6,18 @@
  * Minifies css files and places them into .tmp/public/min directory.
  *
  * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-cssmin
+ *    https://github.com/gruntjs/grunt-contrib-cssmin
  */
 module.exports = function(grunt) {
 
   var version = grunt.file.readJSON('package.json').version;
 
-	grunt.config.set('cssmin', {
-		dist: {
-			src: ['.tmp/public/concat/production.css'],
-			dest: '.tmp/public/min/production.<%= version %>.min.css'
-		}
-	});
+  grunt.config.set('cssmin', {
+    dist: {
+      src: ['.tmp/public/concat/production.css'],
+      dest: '.tmp/public/min/production.' + version + '.min.css'
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 };
